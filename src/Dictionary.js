467bdab39a53f4885b381f1db9bce254b6bd1523
gertagosa <121582460+gertagosa@users.noreply.github.com>
2023-03-30 12:02:39 +0100

    Rendered the synonyms

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -13,9 +13,9 @@ export default function Dictionary() {
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
 
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    // documentation: https://dictionaryapi.dev/e
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
